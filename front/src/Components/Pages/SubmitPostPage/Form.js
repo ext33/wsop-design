6ef0dd19370ab6dfae53e0a5cdcc821d81ffd65f
Egor Bekker <redwoodx3@gmail.com>
2021-02-09 02:09:36 +0300

    add image preview

--- front/src/Components/Pages/SubmitPostPage/Form.js
+++ front/src/Components/Pages/SubmitPostPage/Form.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function Form (props){
+
+    const [image, setImage] = useState({
+        base64: null,
+        alt: null,
+    })
+
+    useEffect(()=>{
+        document.getElementById('image').addEventListener('change', changeHandler)
+        return ()=>{ document.getElementById('image').removeEventListener('change', changeHandler) }
+    })
+
+    function changeHandler(event){
+        const files = Array.from(event.target.files)
+        files.forEach(file => {
+            const reader = new FileReader()
+            reader.onload = (ev) => {
+                setImage((prevState)=>{
+                    return{
+                        ...prevState,
+                        base64: ev.target.result,
+                        alt: file.name,
+                    }
+                })
+            }
+            reader.readAsDataURL(file)
+        })
+    }
+
+
     return(
         <form>
-            <div className='form-title'>
-                SUBMIT POST
-            </div>
             <div className='form-main'>
             <label>Username</label>
             <input className='input input-main' type={'text'} id={'username'} onChange={props.changeHandler} />
             <label>Email</label>
             <input className='input input-main' type={'email'} id={'email'} onChange={props.changeHandler} />
             <label>Image</label>
+            <div className='imageprev' onClick={()=>{document.getElementById('image').click()}}>
+                { image ? <img className='prev' src={image.base64} alt={image.alt}/> : null }
+            </div>
             <input className='input input-main' type={'file'} id={'image'} onChange={props.changeHandler} />
             <label>Description</label>
             <textarea className='input' id={'description'} onChange={props.changeHandler} />
